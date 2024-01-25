283bf759dcd1305a61a057b0cef5d086199f25e8
pedrokielma <peterkielma@gmail.com>
2024-01-25 18:06:46 +0000

    config handlebars and websockets

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 import express from "express";
 import routerProd from './routes/products.routes.js'
 import routerCats from './routes/carts.routes.js'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import  handlebars from 'express-handlebars'
+const app = express();
+
+import http from 'http'
+
+const server = http.createServer(app);
+
+//Socket
+import { Server } from "socket.io"
+
+const io = new Server(server);
+
 const PORT = 8080;
 
-const app = express();
 
 //MIDDLEWARES
 app.use(express.json());
@@ -13,7 +30,37 @@ app.use(express.urlencoded({extended: true}));
 app.use('/api/products', routerProd)
 app.use('/api/carts', routerCats)
 
+//Public
+app.use(express.static(__dirname+'/public'))
 
-app.listen(PORT, () => {
-  console.log("server run on port ", PORT);
-});
+//Views
+app.engine('handlebars', handlebars.engine())
+app.set('view engine', 'handlebars')
+app.set('views', __dirname+'/views')
+
+let messages = []
+
+//page
+
+app.get('/', (req, res)=>{
+  res.render('home', {})
+})
+
+//Inicializar el Socket en el servido
+io.on('connection', (socket)=>{
+  console.log('User conectado')
+  //socket.emit('mesagge', 'Hola Cliente, soy el back')
+  socket.emit('messages', messages)
+  socket.on('new-message', (data)=>{
+    console.log(data)
+    messages.push(data)
+    io.sockets.emit('messages', messages)
+  })
+})
+
+
+
+
+server.listen(PORT, ()=>{
+  console.log('Server running on port 8080')
+})
